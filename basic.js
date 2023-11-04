7454ac9d3003d88cf635cfe297207ecc09f4fac4
meeeeeeeep1 <75493196+meeeeeeeep1@users.noreply.github.com>
2023-11-04 14:13:01 -0400

    Create basic.js

---
+++ basic.js
@@ -0,0 +1,7 @@
+var link = document.querySelector("link[rel~='icon']");
+if (!link) {
+    link = document.createElement('link');
+    link.rel = 'icon';
+    document.head.appendChild(link);
+}
+link.href = 'https://meeeeeeeep1.github.io/haveniscool/favicon.ico';
